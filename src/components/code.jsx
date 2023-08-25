7c133f041571a6c256022eae7b9d1f4c08f5aa2f
GouravKumar06 <gka98963@gmail.com>
2023-08-25 11:48:08 +0530

    first commit

---
+++ src/components/code.jsx
@@ -0,0 +1,50 @@
+import { useContext } from 'react';
+import React from 'react';
+import Editor from './Editor';
+
+import { Box, styled } from '@mui/material';
+
+import { DataContext } from '../context/DataProvider';
+
+const Container = styled(Box)`
+    background-color: #060606;
+    height: 50vh;
+    display: flex;
+`
+
+const Code = () => {
+
+    const { html, css, js, setHtml, setCss, setJs } = useContext(DataContext);
+
+  return (
+    <Container>
+        
+        <Editor 
+            heading="HTML"
+            icon='/'
+            color='#FF3C41'
+            value={html}
+            onChange={setHtml}
+        />
+
+        <Editor
+            heading="Css"
+            icon='*'
+            color='#0EBEFF'
+            value={css}
+            onChange={setCss}
+        />
+
+        <Editor
+            heading="Javascript"
+            icon='( )'
+            color='#FCD000'
+            value={js}
+            onChange={setJs}
+        /> 
+
+    </Container>
+  )
+}
+
+export default Code;
